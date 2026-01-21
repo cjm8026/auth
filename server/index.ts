4aa393dd53eb1ff6b5e3f3a99e01b933410f5982
cjm8026 <ekfrml1@gmail.com>
2026-01-21 23:29:04 +0900

    Add DELETE method to CORS allowed methods

--- server/index.ts
+++ server/index.ts
@@ -37,6 +37,8 @@ app.use(cors({
     'https://api.aws11.shop',
   ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json({ limit: '10mb' }));
